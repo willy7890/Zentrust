1168b298f67f5696225a3c9f7b5f64f6ac34a263
mtundudev <dottomtundu9@gmail.com>
2026-09-19 19:56:56 +0300

    feat:project fundamentals

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,1 +1,8 @@
-// Server entry point – starts the Node.js HTTP server
+require("dotenv").config();
+import app from "./app";
+
+const PORT= Number(process.env.PORT) || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running in ${PORT}`)
+});
